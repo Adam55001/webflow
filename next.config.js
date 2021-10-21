6e506dbad9ebb93984f7ca705ba28e2845bfb1d3
lookup <admin@getlookup.co>
2021-10-21 13:58:54 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,28 +1,18 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
-        source: '/team',
-        destination: '/about',
-      },
-      {
-        source: '/about-us',
-        destination: '/about',
-      },
-      // Path Matching - will match `/post/a` but not `/post/a/b`
-      {
-        source: '/post/:slug',
-        destination: '/news/:slug',
-      },
-      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
-      {
-        source: '/blog/:slug*',
-        destination: '/news/:slug*',
-      },
-      // Rewriting to an external URL
-      {
-        source: '/docs/:slug',
-        destination: 'http://example.com/docs/:slug',
+        source: '/:slug',
+        destination: 'https://rk-shop.webflow.io/:slug',
       },
     ]
   },
