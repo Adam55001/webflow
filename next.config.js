c9ff448cb7867f9689b655f3102816d3acc37c14
rkweb design <92909488+rkflow@users.noreply.github.com>
2021-10-28 12:15:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/',
+        destination: 'https://pf-agri.webflow.io',
+      },
+      {
         source: '/:slug',
         destination: 'https://pf-agri.webflow.io/:slug',
       },
